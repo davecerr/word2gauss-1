bed42d7bc82bfcc6a1e986a9c5da301a37eb8726
davide <davide@pop-os.localdomain>
2020-11-29 16:09:57 +0000

    typo

--- updated_train.py
+++ updated_train.py
@@ -95,7 +95,7 @@ def main(args):
         pickle_in = open("corpus.pkl","rb")
         corpus = pkl.load(pickle_in)
         end = time()
-        print(f"loaded in {round(end - start,2) secs}")
+        print(f"loaded in {round(end - start,2)} secs")
     else:
         print("loading from gzip files")
         files = []
